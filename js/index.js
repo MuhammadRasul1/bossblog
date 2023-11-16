3dd35c7f98d3a964a2425ce5d1fcaf9d3d8d2997
MuhammadRasul1 <elyorjonxxxxx@gmail.com>
2023-11-16 19:30:16 +0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,462 @@
+const data = {
+    posts: [
+      {
+        userId: 1,
+        id: 1,
+        title: "Post 1",
+        body: "<p>Постановлением Кабинета Министров № 294 утверждены Положение о Молодежной индустриально-предпринимательской зоне и Фонде поддержки молодых предпринимателей</p>",
+        created_at: "2023-09-09T11:36:26.861Z",
+        views: 326,
+        image: "https://picsum.photos/360/300/?random=1"
+      },
+      {
+        userId: 2,
+        id: 2,
+        title: "Post 2",
+        body: "<p>Постановлением Кабинета Министров № 294 утверждены Положение о Молодежной индустриально-предпринимательской зоне и Фонде поддержки молодых предпринимателей</p>",
+        created_at: "2023-09-09T11:36:26.861Z",
+        views: 326,
+        image: "https://picsum.photos/360/300/?random=2"
+      },
+      {
+        userId: 3,
+        id: 3,
+        title: "Post 3",
+        body: "<p>Постановлением Кабинета Министров № 294 утверждены Положение о Молодежной индустриально-предпринимательской зоне и Фонде поддержки молодых предпринимателей</p>",
+        created_at: "2023-09-09T11:36:26.861Z",
+        views: 326,
+        image: "https://picsum.photos/360/300/?random=3"
+      },
+      {
+        userId: 4,
+        id: 4,
+        title: "Post 4",
+        body: "<p>Постановлением Кабинета Министров № 294 утверждены Положение о Молодежной индустриально-предпринимательской зоне и Фонде поддержки молодых предпринимателей</p>",
+        created_at: "2023-09-09T11:36:26.861Z",
+        views: 326,
+        image: "https://picsum.photos/360/300/?random=4"
+      },
+      {
+        userId: 5,
+        id: 5,
+        title: "Post 5",
+        body: "<p>Постановлением Кабинета Министров № 294 утверждены Положение о Молодежной индустриально-предпринимательской зоне и Фонде поддержки молодых предпринимателей</p>",
+        created_at: "2023-09-09T11:36:26.861Z",
+        views: 326,
+        image: "https://picsum.photos/360/300/?random=5"
+      },
+      {
+        userId: 6,
+        id: 6,
+        title: "Post 6",
+        body: "<p>Постановлением Кабинета Министров № 294 утверждены Положение о Молодежной индустриально-предпринимательской зоне и Фонде поддержки молодых предпринимателей</p>",
+        created_at: "2023-09-09T11:36:26.861Z",
+        views: 326,
+        image: "https://picsum.photos/360/300/?random=6"
+      },
+      {
+        userId: 7,
+        id: 7,
+        title: "Post 7",
+        body: "<p>Постановлением Кабинета Министров № 294 утверждены Положение о Молодежной индустриально-предпринимательской зоне и Фонде поддержки молодых предпринимателей</p>",
+        created_at: "2023-09-09T11:36:26.861Z",
+        views: 326,
+        image: "https://picsum.photos/360/300/?random=7"
+      },
+      {
+        userId: 8,
+        id: 8,
+        title: "Post 8",
+        body: "<p>Постановлением Кабинета Министров № 294 утверждены Положение о Молодежной индустриально-предпринимательской зоне и Фонде поддержки молодых предпринимателей</p>",
+        created_at: "2023-09-09T11:36:26.861Z",
+        views: 326,
+        image: "https://picsum.photos/360/300/?random=8"
+      },
+      {
+        userId: 9,
+        id: 9,
+        title: "Post 9",
+        body: "<p>Постановлением Кабинета Министров № 294 утверждены Положение о Молодежной индустриально-предпринимательской зоне и Фонде поддержки молодых предпринимателей</p>",
+        created_at: "2023-09-09T11:36:26.861Z",
+        views: 326,
+        image: "https://picsum.photos/360/300/?random=9"
+      },
+      {
+        userId: 10,
+        id: 10,
+        title: "Post 10",
+        body: "<p>Постановлением Кабинета Министров № 294 утверждены Положение о Молодежной индустриально-предпринимательской зоне и Фонде поддержки молодых предпринимателей</p>",
+        created_at: "2023-09-09T11:36:26.861Z",
+        views: 326,
+        image: "https://picsum.photos/360/300/?random=10"
+      },
+      {
+        userId: 11,
+        id: 11,
+        title: "Post 11",
+        body: "<p>Постановлением Кабинета Министров № 294 утверждены Положение о Молодежной индустриально-предпринимательской зоне и Фонде поддержки молодых предпринимателей</p>",
+        created_at: "2023-09-09T11:36:26.861Z",
+        views: 326,
+        image: "https://picsum.photos/360/300/?random=11"
+      },
+      {
+        userId: 12,
+        id: 12,
+        title: "Post 12",
+        body: "<p>Постановлением Кабинета Министров № 294 утверждены Положение о Молодежной индустриально-предпринимательской зоне и Фонде поддержки молодых предпринимателей</p>",
+        created_at: "2023-09-09T11:36:26.861Z",
+        views: 326,
+        image: "https://picsum.photos/360/300/?random=12"
+      },
+      {
+        userId: 1694421712678,
+        title: "Lorem Ipsum Dolor SIt emet",
+        body: "lasdf;aoksdf;alskjdfa;skdfja;slkdfja;sldkfjas;ldkfja;sldkfjas;dlkfj",
+        image: "/static/media/article.6e77bd5bce0396b4a36f.png",
+        created_at: "2023-09-11T08:41:52.678Z",
+        views: 354,
+        id: 13
+      },
+      {
+        userId: 1694421863575,
+        title: "asdfasdf",
+        body: "afsddfsa",
+        image: "/static/media/article.6e77bd5bce0396b4a36f.png",
+        created_at: "2023-09-11T08:44:23.575Z",
+        views: 354,
+        id: 14
+      }
+    ],
+    users: [
+      {
+        id: 1,
+        username: "user1",
+        password: "123",
+        name: "Leanne Graham",
+        email: "Sincere@april.biz",
+        phone: "1-770-736-8031 x56442",
+        image: "https://picsum.photos/360/300/?random=1"
+      },
+      {
+        id: 2,
+        username: "user2",
+        password: "123",
+        name: "Ervin Howell",
+        email: "Shanna@melissa.tv",
+        phone: "010-692-6593 x09125",
+        image: "https://picsum.photos/360/300/?random=2"
+      },
+      {
+        id: 3,
+        username: "user3",
+        password: "123",
+        name: "Clementine Bauch",
+        email: "Nathan@yesenia.net",
+        phone: "1-463-123-4447",
+        image: "https://picsum.photos/360/300/?random=3"
+      },
+      {
+        id: 4,
+        username: "user4",
+        password: "123",
+        name: "Patricia Lebsack",
+        email: "Julianne.OConner@kory.org",
+        phone: "493-170-9623 x156",
+        image: "https://picsum.photos/360/300/?random=4"
+      },
+      {
+        id: 5,
+        username: "user5",
+        password: "123",
+        name: "Chelsey Dietrich",
+        email: "Lucio_Hettinger@annie.ca",
+        phone: "(254)954-1289",
+        image: "https://picsum.photos/360/300/?random=5"
+      },
+      {
+        id: 6,
+        username: "user6",
+        password: "123",
+        name: "Mrs. Dennis Schulist",
+        email: "Karley_Dach@jasper.info",
+        phone: "1-477-935-8478 x6430",
+        image: "https://picsum.photos/360/300/?random=6"
+      },
+      {
+        id: 7,
+        username: "user7",
+        password: "123",
+        name: "Kurtis Weissnat",
+        email: "Telly.Hoeger@billy.biz",
+        phone: "210.067.6132",
+        image: "https://picsum.photos/360/300/?random=7"
+      },
+      {
+        id: 8,
+        username: "user8",
+        password: "123",
+        name: "Nicholas Runolfsdottir V",
+        email: "Sherwood@rosamond.me",
+        phone: "586.493.6943 x140",
+        image: "https://picsum.photos/360/300/?random=8"
+      },
+      {
+        id: 9,
+        username: "user9",
+        password: "123",
+        name: "Glenna Reichert",
+        email: "Chaim_McDermott@dana.io",
+        phone: "(775)976-6794 x41206",
+        image: "https://picsum.photos/360/300/?random=9"
+      },
+      {
+        id: 10,
+        username: "user10",
+        password: "123",
+        name: "Clementina DuBuque",
+        email: "Rey.Padberg@karina.biz",
+        phone: "024-648-3804",
+        image: "https://picsum.photos/360/300/?random=10"
+      },
+      {
+        id: 11,
+        username: "user11",
+        password: "123",
+        name: "Stefano Richi",
+        email: "steffi@karina.biz",
+        phone: "024-648-3804",
+        image: "https://picsum.photos/360/300/?random=11"
+      },
+      {
+        id: 12,
+        username: "user12",
+        password: "123",
+        name: "Fernadona DuBuque",
+        email: "ferrri@karina.biz",
+        phone: "024-648-3804",
+        image: "https://picsum.photos/360/300/?random=12"
+      },
+      {
+        id: 1694252587266,
+        username: "user1694252587266",
+        password: "123",
+        name: "Mrs. Dennis Schulist",
+        email: "Karley_Dach@jasper.info",
+        phone: "1-477-935-8478 x6430",
+        image: "https://picsum.photos/360/300/?random=6"
+      },
+      {
+        id: 1694252664022,
+        username: "user1694252664022",
+        password: "123",
+        name: "Mrs. Dennis Schulist",
+        email: "Karley_Dach@jasper.info",
+        phone: "1-477-935-8478 x6430",
+        image: "https://picsum.photos/360/300/?random=6"
+      },
+      {
+        id: 1694252668581,
+        username: "user1694252668581",
+        password: "123",
+        name: "Mrs. Dennis Schulist",
+        email: "Karley_Dach@jasper.info",
+        phone: "1-477-935-8478 x6430",
+        image: "https://picsum.photos/360/300/?random=6"
+      },
+      {
+        username: "user123123",
+        password: "123"
+      }
+    ],
+    avatars: [
+      {
+        id: 1,
+        url: "https://raw.githubusercontent.com/mukhammadyn/product-images/main/user-images/image-zena.jpg"
+      },
+      {
+        id: 2,
+        url: "https://raw.githubusercontent.com/mukhammadyn/product-images/main/user-images/image-elijah.jpg"
+      },
+      {
+        id: 3,
+        url: "https://raw.githubusercontent.com/mukhammadyn/product-images/main/user-images/image-george.jpg"
+      },
+      {
+        id: 4,
+        url: "https://raw.githubusercontent.com/mukhammadyn/product-images/main/user-images/image-james.jpg"
+      },
+      {
+        id: 5,
+        url: "https://raw.githubusercontent.com/mukhammadyn/product-images/main/user-images/image-javier.jpg"
+      },
+      {
+        id: 6,
+        url: "https://raw.githubusercontent.com/mukhammadyn/product-images/main/user-images/image-roxanne.jpg"
+      },
+      {
+        id: 7,
+        url: "https://raw.githubusercontent.com/mukhammadyn/product-images/main/user-images/image-ryan.jpg"
+      }
+    ],
+    history: [
+      {
+        id: 1,
+        isViewed: false,
+        url: "https://raw.githubusercontent.com/mukhammadyn/product-images/main/user-images/image-zena.jpg"
+      },
+      {
+        id: 2,
+        isViewed: true,
+        url: "https://raw.githubusercontent.com/mukhammadyn/product-images/main/user-images/image-elijah.jpg"
+      },
+      {
+        id: 3,
+        isViewed: false,
+        url: "https://raw.githubusercontent.com/mukhammadyn/product-images/main/user-images/image-george.jpg"
+      },
+      {
+        id: 4,
+        isViewed: false,
+        url: "https://raw.githubusercontent.com/mukhammadyn/product-images/main/user-images/image-james.jpg"
+      },
+      {
+        id: 5,
+        isViewed: true,
+        url: "https://raw.githubusercontent.com/mukhammadyn/product-images/main/user-images/image-javier.jpg"
+      },
+      {
+        id: 6,
+        isViewed: false,
+        url: "https://raw.githubusercontent.com/mukhammadyn/product-images/main/user-images/image-roxanne.jpg"
+      },
+      {
+        id: 7,
+        isViewed: false,
+        url: "https://raw.githubusercontent.com/mukhammadyn/product-images/main/user-images/image-ryan.jpg"
+      }
+    ]
+  }
+
+
+let elList = document.querySelector(".new-article__list")
+
+for(let i = 0; i < 4; i++) {
+    let elNewItem = document.createElement("li")
+    elNewItem.setAttribute("class", "new-article__item")
+
+    let elNewImg = document.createElement("img")
+    elNewImg.setAttribute("src", data.posts[i].image)
+    elNewImg.setAttribute("class", "new-article__img")
+
+    let elNewWrapper = document.createElement("div")
+    elNewWrapper.setAttribute("class", "new-article__wrapper")
+
+    let elNewLinkWrapper = document.createElement("div")
+    elNewLinkWrapper.setAttribute("class", "new-article__link-wrapper")
+
+    let elNewLink = document.createElement("a")
+    elNewLink.textContent = data.posts[i].id
+    elNewLink.setAttribute("class", "new-article__link")
+
+    let elNewData = document.createElement("p")
+    elNewData.textContent = data.posts[i].created_at
+    elNewData.setAttribute("class", "new-article__data")
+
+    let elNewSubtitle = document.createElement("h3")
+    elNewSubtitle.textContent = data.posts[i].title
+    elNewSubtitle.setAttribute("class", "new-article__subtitle")
+
+    let elNewText = document.createElement("p")
+    elNewText.textContent = data.posts[i].body
+    elNewText.setAttribute("class", "new-article__text")
+
+    let elNewUserWrapper = document.createElement("div")
+    elNewUserWrapper.setAttribute("class", "new-article__user-wrapper")
+
+    let elNewUser = document.createElement("div")
+    elNewUser.setAttribute("class", "new-article__user")
+
+    let elNewUserImg = document.createElement("img")
+    elNewUserImg.setAttribute("src", data.avatars[i].url)
+    elNewUserImg.setAttribute("class", "new-article__user-img")
+
+    let elNewUserName = document.createElement("p")
+    elNewUserName.textContent = data.users[i].name
+    elNewUserName.setAttribute("class", "new-article__user-name")
+
+    let elNewComments = document.createElement("div")
+    elNewComments.setAttribute("class", "new-article__comments")
+
+    let elNewBtnFavourites = document.createElement("button")
+    elNewBtnFavourites.textContent = "Keyinroq oqish" 
+    elNewBtnFavourites.setAttribute("class", "new-article__btn-favourites")
+    
+    let elNewBtnFavouritesImg = document.createElement("img")
+    elNewBtnFavouritesImg.setAttribute("src", "img/favorites.svg")
+    elNewBtnFavouritesImg.setAttribute("class", "new-article__btn-favourites-img")
+
+    let elNewTimeReading = document.createElement("p")
+    elNewTimeReading.textContent = "3 daqiqa oqish"
+    elNewTimeReading.setAttribute("class", "new-article__time-reading")
+    
+    let elNewTimeReadingImg = document.createElement("img")
+    elNewTimeReadingImg.setAttribute("src", "img/time.svg")
+    elNewTimeReadingImg.setAttribute("alt", "time")
+    elNewTimeReadingImg.setAttribute("class", "new-article__time-reading-img")
+    
+    let elNewBtnComments = document.createElement("button")
+    elNewBtnComments.textContent = "34"
+    elNewBtnComments.setAttribute("class", "new-article__btn-comments")
+
+    let elNewBtnCommentsImg = document.createElement("img")
+    elNewBtnCommentsImg.setAttribute("src", "img/comments.svg")
+    elNewBtnCommentsImg.setAttribute("alt", "comments")
+    elNewBtnCommentsImg.setAttribute("class", "new-article__btn-comments-img")
+
+    let elNewViews = document.createElement("p")
+    elNewViews.textContent = "comments"
+    elNewViews.setAttribute("class", "new-article__views")
+    
+    let elNewViewsImg = document.createElement("img")
+    elNewViewsImg.setAttribute("src", "img/views.svg")
+    elNewViewsImg.setAttribute("alt", "views")
+    elNewViewsImg.setAttribute("class", "new-article__time-reading-img")
+
+
+    //new-article__user
+    elNewUser.appendChild(elNewUserImg)
+    elNewUser.appendChild(elNewUserName)
+
+    //new-article__button
+    elNewBtnFavourites.appendChild(elNewBtnFavouritesImg)
+    elNewTimeReading.appendChild(elNewTimeReadingImg)
+    elNewBtnComments.appendChild(elNewBtnCommentsImg)
+    elNewViews.appendChild(elNewViewsImg)
+    
+    //new-article__comments
+    elNewComments.appendChild(elNewBtnFavourites)
+    elNewComments.appendChild(elNewTimeReading)
+    elNewComments.appendChild(elNewBtnComments)
+    elNewComments.appendChild(elNewViews)
+
+    //new-article__user-wrapper
+    elNewUserWrapper.appendChild(elNewUser)
+    elNewUserWrapper.appendChild(elNewComments)
+
+    //new-article__link-wrappper
+    elNewLinkWrapper.appendChild(elNewLink)
+    elNewLinkWrapper.appendChild(elNewData)
+
+    //new-article__wrapper
+    elNewWrapper.appendChild(elNewLinkWrapper)
+    elNewWrapper.appendChild(elNewSubtitle)
+    elNewWrapper.appendChild(elNewText)
+    elNewWrapper.appendChild(elNewUserWrapper)
+
+    //new-article__item
+    elNewItem.appendChild(elNewImg)
+    elNewItem.appendChild(elNewWrapper)
+
+    //new-article__list
+    elList.appendChild(elNewItem)
+}
